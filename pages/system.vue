73cd1c32c1acf6a7dee658fec09ea53dd66431e1
Ilya Bakirov <everjupe13@gmail.com>
2023-10-20 22:47:17 +0300

    feat: change system page

---
+++ pages/system.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { useHead } from 'nuxt/app'
+
+import {
+  SystemAreasInfoScreen,
+  SystemDiagramScreen,
+  SystemFeaturesScreen
+} from '@/components/pages/system'
+import { BreadCrumbs } from '@/components/widgets/bread-crumbs'
+
+useHead({
+  title: 'Product'
+})
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <BreadCrumbs :crumbs="[{ label: 'How it works' }]" class="mb-30 mt-30" />
+    </div>
+    <SystemDiagramScreen />
+    <SystemFeaturesScreen />
+    <SystemAreasInfoScreen />
+  </div>
+</template>

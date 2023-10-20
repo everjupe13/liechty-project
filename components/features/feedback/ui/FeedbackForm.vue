c4a1eb639af02b254b08a7e301b71994e148f2d1
Ilya Bakirov <everjupe13@gmail.com>
2023-10-20 22:39:28 +0300

    feat: add UI feedback form

---
+++ components/features/feedback/ui/FeedbackForm.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { AppInput } from '@/components/shared/input'
+</script>
+
+<template>
+  <form @submit.prevent>
+    <AppInput class="mb-20" placeholder="Your name" />
+    <AppInput class="mb-20" placeholder="Phone number" />
+    <AppInput class="mb-20" placeholder="Your e-mail" />
+  </form>
+</template>

a433b48b3f0a38172161a1f4dfc779f4534b055d
Ilya Bakirov <everjupe13@gmail.com>
2023-11-01 20:12:00 +0300

    feat: update header/footer

--- components/widgets/footer/ui/AppFooter.vue
+++ components/widgets/footer/ui/AppFooter.vue
@@ -5,18 +5,18 @@ import { NavMenu } from '@/components/shared/nav-menu'
 
 <template>
   <footer id="footer">
-    <div class="border-t border-dark/20 pb-60 pt-40">
+    <div class="border-t border-dark/20 pb-60 pt-40 md:pb-40 md:pt-30">
       <div class="container">
-        <div class="flex justify-between">
-          <AppLogoExpanded />
-          <NavMenu />
+        <div class="flex justify-between md:flex-col">
+          <AppLogoExpanded icon-classes="md:w-[100px] md:h-auto" />
+          <NavMenu class="md:flex-col md:gap-y-10" />
         </div>
       </div>
     </div>
 
-    <div class="border-t border-dark/20 py-40">
+    <div class="border-t border-dark/20 py-40 md:py-20">
       <div class="container">
-        <div class="flex items-center justify-between">
+        <div class="flex items-center justify-between md:flex-col md:gap-y-5">
           <p class="leading-none text-dark/60 text-14">
             © Liechty 2023. All rights reserved
           </p>
--- components/widgets/header/ui/AppHeader.vue
+++ components/widgets/header/ui/AppHeader.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
 import { AppLogo } from '@/components/shared/logo'
 import { NavMenu } from '@/components/shared/nav-menu'
+import { useModalMenu } from '@/components/widgets/dialog'
+
+const { openModalMenu } = useModalMenu()
 </script>
 
 <template>
   <header
     id="header"
-    class="group flex h-85 items-center border-b border-dark/20"
+    class="group flex h-85 items-center border-b border-dark/20 md:h-50"
   >
     <div class="container">
       <div class="flex items-center justify-between">
-        <NavMenu class="flex-grow" />
+        <NavMenu class="flex-grow md:hidden" />
         <AppLogo class="flex-shrink-0" />
+        <button class="hidden items-center justify-center p-5 md:flex">
+          <img
+            src="@/assets/img/burger-icon.svg"
+            alt=""
+            @click="openModalMenu"
+          />
+        </button>
       </div>
     </div>
   </header>

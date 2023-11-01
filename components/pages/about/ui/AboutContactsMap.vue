65c3c5c48e2a41252c1d7469a3597acd33f021a9
Ilya Bakirov <everjupe13@gmail.com>
2023-11-01 20:08:36 +0300

    feat: add map

--- components/pages/about/ui/AboutContactsMap.vue
+++ components/pages/about/ui/AboutContactsMap.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex min-h-[520px] items-center justify-center">
     <img
-      src="/images/about/map.jpg"
+      src="/images/about/map.png"
       class="pointer-events-none block h-full select-none object-cover"
     />
   </div>

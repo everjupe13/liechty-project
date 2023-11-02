5abef69baddee6087be0d28763ee85915897a52a
Ilya Bakirov <everjupe13@gmail.com>
2023-11-02 11:04:35 +0300

    feat: change mock diagram texts

--- components/widgets/diagram/model/mock-data.ts
+++ components/widgets/diagram/model/mock-data.ts
@@ -3,56 +3,56 @@ export const DiagramData = [
     id: 1,
     name: 'Inlet water',
     description:
-      '<p>Lorem ipsum dolor sit amet consectetur. Viverra et vitae porttitor eu ornare mauris arcu ultricies integer. Eget congue ac enim consequat dignissim sit sit. Urna egestas tristique etiam semper varius et convallis. Eu pellentesque praesent tortor pellentesque rhoncus pulvinar. Elit leo luctus pellentesque cursus vitae amet eget pharetra</p>',
-    title: '<h3>High-pressure trunk line</h3>',
+      '<p>Water is supplied automatically from the water supply.</p>',
+    title: '<h3></h3>',
     image: '/images/system/diagram1.png'
   },
   {
     id: 2,
     name: 'Preliminary purification',
     description:
-      '<p>Lorem ipsum dolor sit amet consectetur. Viverra et vitae porttitor eu ornare mauris arcu ultricies integer. Eget congue ac enim consequat dignissim sit sit. Urna egestas tristique etiam semper varius et convallis. Eu pellentesque praesent tortor pellentesque rhoncus pulvinar.</p>',
-    title: '<h3>Trunk line</h3>',
+      '<p>First of all, water passes through pre-filtration (sediment, microcarbon and ultrafilter), which allows to purify water from mechanical and chemical impurities. </p>',
+    title: '<h3></h3>',
     image: '/images/system/diagram2.png'
   },
   {
     id: 3,
     name: 'Central module',
     description:
-      '<p>Lorem ipsum dolor sit amet consectetur. Viverra et vitae porttitor eu ornare mauris arcu ultricies integer. Eget congue ac enim consequat dignissim sit sit. Urna egestas tristique etiam semper varius et convallis.Elit leo luctus pellentesque cursus vitae amet eget pharetra</p>',
-    title: '<h3>Trunk trunk</h3>',
+      '<p>The heart of the system. Here water is finely purified by a double reverse osmosis and disinfected in a volumetric UV sterilization tank. Then purified water is then injected under high pressure by a silent pump into the high-pressure hoseline. In addition, the central module has a controller that coordinates the operation of the nozzles and control units.</p>',
+    title: '<h3></h3>',
     image: '/images/system/diagram3.png'
   },
   {
     id: 4,
     name: 'HP hoseline',
     description:
-      '<p>Lorem ipsum dolor sit amet consectetur. Viverra et vitae porttitor eu ornare mauris arcu ultricies integer. Eu pellentesque praesent tortor pellentesque rhoncus pulvinar. Elit leo luctus pellentesque cursus vitae amet eget pharetra</p>',
-    title: '<h3>Pressure trunk line</h3>',
+      '<p>The high-pressure hoseline is designed as a loop. It consists of a double-layer hose (inner layer material made of PTFE, outer layer is a stainless steel braiding), high-pressure fittings and tees. The operational pressure in the system ranges between 35 - 82 bar, while the burst pressure of the hose is 900 bar, in other words, it has a 15 times safety margin. The material of the inner layer is not accidentally chosen, PTFE has exceptional low adhesion and is not a food for bacteria.</p>',
+    title: '<h3></h3>',
     image: '/images/system/diagram4.png'
   },
   {
     id: 5,
     name: 'Signal cable to SV noiseless valves',
     description:
-      '<p>Lorem ipsum dolor sit amet consectetur. Viverra et vitae porttitor eu ornare mauris arcu ultricies integer. Elit leo luctus pellentesque cursus vitae amet eget pharetra</p>',
-    title: '<h3>High trunk line</h3>',
+      '<p>SV noiseless valves are powered by a low-voltage cable. Each cable from its own valve is led to the controller installed in the central module.</p>',
+    title: '<h3></h3>',
     image: '/images/system/diagram5.png'
   },
   {
     id: 6,
     name: 'Control units',
     description:
-      '<p>Eu ornare mauris arcu ultricies integer. Eget congue ac enim consequat dignissim sit sit. Urna egestas tristique etiam semper varius et convallis. Eu pellentesque praesent tortor pellentesque rhoncus pulvinar. Elit leo luctus pellentesque cursus vitae amet eget pharetra</p>',
-    title: '<h3>High-pressure line</h3>',
+      '<p>Liechty control units can be either wired or wireless. All control units are equipped with humidity sensor of high accuracy, which measures RH level in the room and gives a command to switch on the nozzles in case the RH level is lower than the set one and vice versa.</p>',
+    title: '<h3></h3>',
     image: '/images/system/diagram6.png'
   },
   {
     id: 7,
     name: 'Miniature nozzles',
     description:
-      '<p>Get congue ac enim consequat dignissim sit sit. Urna egestas tristique etiam semper varius et convallis. Eu pellentesque praesent tortor pellentesque rhoncus pulvinar. Elit leo luctus pellentesque cursus vitae amet eget pharetra</p>',
-    title: '<h3>High-pressure trunk</h3>',
+      '<p>The nozzle heads are installed directly into the room through ventilation grilles, walls or ceiling and atomize a fog into the room. Microdroplets instantly dissolve into the air, increasing RH level. The miniature nozzle heads do not spoil the interior design.</p>',
+    title: '<h3></h3>',
     image: '/images/system/diagram7.png'
   }
 ]

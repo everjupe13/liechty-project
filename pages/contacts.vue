a93804c157c2131a3f7c524a2419c34664237705
Ilya Bakirov <everjupe13@gmail.com>
2023-11-09 00:14:38 +0300

    feat: app page

---
+++ pages/contacts.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { useHead } from 'nuxt/app'
+
+import { BreadCrumbs } from '@/components/widgets/bread-crumbs'
+import { ContactsScreen } from '@/components/widgets/contacts'
+
+useHead({
+  title: 'Liechty - Air humidification system'
+})
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <BreadCrumbs
+        :crumbs="[{ label: 'Contacts' }]"
+        class="mb-60 mt-30 md:mb-40"
+      />
+    </div>
+    <ContactsScreen />
+  </div>
+</template>

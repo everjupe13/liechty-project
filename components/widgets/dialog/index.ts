5d6d9dc5a97b19bb1b8fcac96366299da5f2c4cb
Ilya Bakirov <everjupe13@gmail.com>
2023-11-09 00:13:14 +0300

    feat: add re-export for api

--- components/widgets/dialog/index.ts
+++ components/widgets/dialog/index.ts
@@ -1,4 +1,5 @@
 export { useModalMenu } from './model/useModalMenu'
 export { usePurchaseDetailsModal } from './model/usePurchaseDetailsModal'
+export { useSuccessModal } from './model/useSuccessModal'
 export { default as AppDialog } from './ui/AppDialog.vue'
 export { default as PurchaseDetails } from './ui/PurchaseDetails.vue'

16ef086586dc89e5ba6f18814c6b4e84d0a3f92d
Ilya Bakirov <everjupe13@gmail.com>
2023-11-09 00:13:36 +0300

    feat: add privacy page

---
+++ components/pages/privacy-policy/ui/PrivacyPolicyScreen.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="pb-60 pt-30">
+    <div class="container">
+      <div class="mb-40">
+        <h2 class="section-title">Privacy policy</h2>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ pages/privacy-policy.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useHead } from 'nuxt/app'
+
+import { PrivacyPolicyScreen } from '@/components/pages/privacy-policy'
+import { BreadCrumbs } from '@/components/widgets/bread-crumbs'
+
+useHead({
+  title: 'Liechty - Air humidification system'
+})
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <BreadCrumbs
+        :crumbs="[{ label: 'Privacy Policy' }]"
+        class="mb-30 mt-30 md:mb-15"
+      />
+    </div>
+    <PrivacyPolicyScreen />
+  </div>
+</template>

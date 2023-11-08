02c736da88d77b6f9bcefe077e86446c514c2c06
Ilya Bakirov <everjupe13@gmail.com>
2023-11-09 00:18:10 +0300

    feat: change block template (!TEMPORARY / remove comments)

--- components/pages/system/ui/SystemDownloadScreen.vue
+++ components/pages/system/ui/SystemDownloadScreen.vue
@@ -1,4 +1,4 @@
-<script lang="ts" setup>
+<!-- <script lang="ts" setup>
 import { DownloadCard } from '@/components/features/download-card'
 </script>
 
@@ -32,6 +32,43 @@ import { DownloadCard } from '@/components/features/download-card'
           title="License 2023"
           excerpt="Little description of fi rutrum ut imperdiet diam id lobortis hendrerit. Dignissim aliquet ne."
           file-size="24 KB"
+        />
+      </div>
+    </div>
+  </section>
+</template> -->
+
+<script lang="ts" setup>
+import { DownloadCard } from '@/components/features/download-card'
+</script>
+
+<template>
+  <section class="bg-[#F8F9FC] pb-[140px] pt-70 xl:py-80 lg:py-60 md:py-40">
+    <div class="container">
+      <div class="mb-40">
+        <h2 class="section-title">Downloads</h2>
+      </div>
+      <div class="grid grid-cols-3 gap-x-20 md:grid-cols-1 md:gap-10">
+        <DownloadCard
+          design-type="white"
+          title="Main catalogue"
+          excerpt="Here you may find all technical information & description for Liechty products."
+          description="Here you may find all technical information & description for Liechty products."
+          file-size="162 KB"
+        />
+        <DownloadCard
+          design-type="white"
+          title="Booklet"
+          excerpt="Our booklet for an overview of Liechty developments."
+          description="Our booklet for an overview of Liechty developments."
+          file-size="162 KB"
+        />
+        <DownloadCard
+          filled-card
+          title="Booklet"
+          excerpt="Dignissim aliquet ne."
+          description="To request all other documentation about our products, please contact us using the contact details or leave a request in the form below."
+          file-size="162 KB"
         />
       </div>
     </div>

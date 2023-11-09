a83c4497dfbe1c59591f0bfcb23eb4f8e1cfbe5c
Ilya Bakirov <everjupe13@gmail.com>
2023-11-09 12:52:38 +0300

    feat: add GMap

--- components/widgets/contacts/ui/ContactsMap.vue
+++ components/widgets/contacts/ui/ContactsMap.vue
@@ -2,9 +2,13 @@
   <div
     class="flex aspect-[calc(1360/520)] items-center justify-center md:aspect-[calc(1020/480)]"
   >
-    <img
-      src="/images/about/map.jpg"
-      class="pointer-events-none block h-full select-none object-cover"
-    />
+    <iframe
+      class="block h-full w-full grayscale-[0.3]"
+      frameborder="0"
+      scrolling="no"
+      marginheight="0"
+      marginwidth="0"
+      src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=1260%20Clarence%20Avenue,%20tertiary,%20Winnipeg,%20Canada+(Liechty%20Company)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+    ></iframe>
   </div>
 </template>

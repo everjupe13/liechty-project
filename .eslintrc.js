a52760e25a2ef448911f32fea9f2ff4d4b20e897
Ilya Bakirov <everjupe13@gmail.com>
2023-11-12 15:42:13 +0300

    lint: change config

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,6 +42,7 @@ module.exports = {
     'vue/no-multiple-template-root': 'off',
     'vue/no-v-for-template-key': 'off',
     'vue/no-v-html': 'off',
+    'vue/prop-name-casing': 'off',
 
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': [

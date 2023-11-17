df17d80ea042d8a70bf4f13dd3ad344697adb517
Ilya Bakirov <everjupe13@gmail.com>
2023-11-17 20:53:16 +0300

    feat: new video

--- components/pages/home/ui/HomeVideoPlayerScreen.vue
+++ components/pages/home/ui/HomeVideoPlayerScreen.vue
@@ -7,7 +7,7 @@ import { YoutubeVideoPlayer } from '@/components/widgets/video-player'
     <div class="container">
       <div class="flex items-center justify-center">
         <YoutubeVideoPlayer
-          video-src="https://www.youtube-nocookie.com/embed/IdYFK_gz_1E?si=9FLC8F9lu-7H9tct"
+          video-src="https://www.youtube.com/embed/sQiNYFcWOdw?si=s-3W7D2mkaQJPO7C"
         />
       </div>
     </div>

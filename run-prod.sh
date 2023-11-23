a567362505bc62f2e869a262059527498e6d91ae
Ilya Bakirov <everjupe13@gmail.com>
2023-11-23 15:05:56 +0300

    feat: add restart docker policy

--- run-prod.sh
+++ run-prod.sh
@@ -4,4 +4,5 @@ IMAGE_NAME=liechty_front
 CONTAINER_NAME=liechty_front_container
 
 docker build -t $IMAGE_NAME .
-docker run -d --rm -p 3000:3000 --name $CONTAINER_NAME $IMAGE_NAME
+# docker run -d --rm -p 3000:3000 --name $CONTAINER_NAME $IMAGE_NAME
+docker run -d --restart unless-stopped -p 3000:3000 --name $CONTAINER_NAME $IMAGE_NAME

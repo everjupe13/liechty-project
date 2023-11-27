4de14fa8923b237149f828990051b34ca89830f7
Ilya Bakirov <everjupe13@gmail.com>
2023-11-27 13:03:43 +0300

    feat: fix swiper

--- components/pages/product/index.ts
+++ components/pages/product/index.ts
@@ -2,4 +2,5 @@ export { default as ProductDownloadScreen } from './ui/ProductDownloadScreen.vue
 export { default as ProductFAQScreen } from './ui/ProductFAQScreen.vue'
 export { default as ProductGalleryView } from './ui/ProductGalleryView.vue'
 export { default as ProductInfoScreen } from './ui/ProductInfoScreen.vue'
+export { default as ProductModelsSwiperCheckout } from './ui/ProductModelsSwiperCheckout.vue'
 export { default as ProductModelsSwiperScreen } from './ui/ProductModelsSwiperScreen.vue'
--- components/pages/product/ui/ProductModelsSwiperScreen.vue
+++ components/pages/product/ui/ProductModelsSwiperScreen.vue
@@ -1,21 +1,9 @@
 <script lang="ts" setup>
-import { CategoryType } from 'api/categories/types'
-import type { Swiper as ISwiper } from 'swiper'
-import { FreeMode } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/vue'
-import { computed, provide, type Ref, ref } from 'vue'
+import { computed, provide, ref } from 'vue'
 
 import { ModelType } from '@/api/models/types'
-import { useWidthBreakpoints } from '@/composables/useWidthBreakpoints'
 
-import ProductModelsSwiperControls from './ProductModelsSwiperControls.vue'
-import ProductModelsSwiperDesktop from './ProductModelsSwiperDesktop.vue'
-import ProductModelsSwiperMobile from './ProductModelsSwiperMobile.vue'
-
-const { smallerOrEqual } = useWidthBreakpoints()
-
-const isMobileScreen = computed(() => smallerOrEqual('lg').value)
-const modules = !isMobileScreen.value ? [FreeMode] : []
+import ProductModelsSwiperCheckout from './ProductModelsSwiperCheckout.vue'
 
 const isSwiperEnd = ref(false)
 const isSwiperBeginning = ref(true)
@@ -25,44 +13,16 @@ provide('swiperOptions', {
   isSwiperBeginning
 })
 
-const onSlideChange = (swiper: typeof ISwiper) => {
-  isSwiperEnd.value = swiper.isEnd
-  isSwiperBeginning.value = swiper.isBeginning
-  swiperActiveIndex.value = swiper.activeIndex
-}
-const onSwiper = (swiper: typeof ISwiper) => {
-  swiperRef.value = swiper
-}
-
-const swiperRef: Ref<typeof ISwiper | null> = ref(null)
-const swiperActiveIndex = ref(0)
-
 const isLoaded = ref(false)
 
 onMounted(() => {
   isLoaded.value = true
 })
-const SwiperCardComponent = computed(() =>
-  isMobileScreen.value ? ProductModelsSwiperMobile : ProductModelsSwiperDesktop
-)
 
 type Props = {
   data: ModelType | null
 }
 const props = defineProps<Props>()
-const computeModels = (products: CategoryType['products']) => {
-  if (isMobileScreen.value) {
-    return products
-  }
-
-  const newProducts = []
-  for (let i = 0; i < products.length; i += 4) {
-    newProducts.push(products.slice(i, i + 4))
-  }
-
-  return newProducts
-}
-
 const isVisible = computed(
   () =>
     props.data?.categories &&
@@ -82,35 +42,7 @@ const isVisible = computed(
         :key="category.id"
         class="mb-60 last:mb-0"
       >
-        <Swiper
-          v-if="isLoaded && category.products && category.products.length > 0"
-          :space-between="24"
-          :modules="modules"
-          :slides-per-view="1"
-          :slides-per-group="1"
-          @swiper="onSwiper"
-          @slide-change="onSlideChange"
-        >
-          <template #container-start>
-            <div
-              class="mb-40 flex items-center justify-between md:flex-col md:gap-y-20"
-            >
-              <h2 class="section-title">{{ category.name }}</h2>
-              <template v-if="!swiperRef?.isLocked || true">
-                <ProductModelsSwiperControls />
-              </template>
-            </div>
-          </template>
-          <swiper-slide
-            v-for="(item, index) in computeModels(category.products)"
-            :key="index"
-          >
-            <Component
-              :is="SwiperCardComponent"
-              v-bind="isMobileScreen ? item : { data: item }"
-            />
-          </swiper-slide>
-        </Swiper>
+        <ProductModelsSwiperCheckout :category="category" />
       </div>
     </div>
   </section>

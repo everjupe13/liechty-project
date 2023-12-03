198d53740e9a6256c90dc34376e1be39cb2a3128
Ilya Bakirov <everjupe13@gmail.com>
2023-12-04 00:23:32 +0300

    feat: add system i18n

--- components/pages/system/model/mock-data.tsx
+++ components/pages/system/model/mock-data.tsx
@@ -1,58 +1,98 @@
 export const FeaturesCardsData = [
   {
     id: 0,
-    title: '100% hygiene',
-    description:
-      'The unique purification system reduces salt content in spray water by 99.5%, and the volumetric UV sterilization with circulation kills up to 99.99% of microbes.',
+    title: {
+      en: '100% hygiene',
+      de: '100% Hygiene'
+    },
+    description: {
+      en: 'The unique purification system reduces salt content in spray water by 99.5%, and the volumetric UV sterilization with circulation kills up to 99.99% of microbes.',
+      de: 'Das einzigartige Reinigungssystem reduziert den Salzgehalt im Spritzwasser um 99,5%, und die volumetrische UV-Sterilisation mit Zirkulation tötet bis zu 99,99% der Mikroben ab.'
+    },
     image: '/images/system/features/1.svg'
   },
   {
     id: 1,
-    title: 'From one room to whole house',
-    description:
-      'Flexible capacity of Liechty systems allows to humidify premises from 10 to 900 sq. meters. One nozzle may effectively operate in a room from 10 to 50 sq. meters.',
+    title: {
+      en: 'From one room to whole house',
+      de: 'Von einem Zimmer zum ganzen Haus'
+    },
+    description: {
+      en: 'Flexible capacity of Liechty systems allows to humidify premises from 100 to 10,000 sq. ft. One nozzle may effectively operate in a room from 100 to 500 sq. ft.',
+      de: 'Die flexible Kapazität der Liechty-Systeme ermöglicht die Befeuchtung von Räumlichkeiten von 10 bis 900 Quadratmetern. Eine Düse kann effektiv in einem Raum von 10 bis 50 Quadratmetern arbeiten.'
+    },
     image: '/images/system/features/2.svg'
   },
   {
     id: 2,
-    title: 'Energy efficiency',
-    description:
-      'One of the main advantages of Liechty system is it’s low power comsumption. The main power consumer is the high-pressure pump, with a total power consumption of less than 330 watts at full load.',
+    title: {
+      en: 'Energy efficiency',
+      de: 'Energieeffizienz'
+    },
+    description: {
+      en: 'One of the main advantages of Liechty system is it’s low power comsumption. The main power consumer is the high-pressure pump, with a total power consumption of less than 330 watts at full load.',
+      de: 'Einer der Hauptvorteile des Liechty-Systems ist sein geringer Stromverbrauch. Der Hauptstromverbraucher ist die Hochdruckpumpe mit einer Gesamtstromverbrauch von weniger als 330 Watt bei Volllast.'
+    },
     image: '/images/system/features/3.svg'
   },
   {
     id: 3,
-    title: 'Long service lifetime',
-    description:
-      'Due to modern technological solutions, our equipment has an extremely long service interval - only once every two years. The equipment may be maintained at place of use.',
+    title: {
+      en: 'Long service lifetime',
+      de: 'Lange Lebensdauer'
+    },
+    description: {
+      en: 'Due to modern technological solutions, our equipment has an extremely long service interval - only once every two years. The equipment may be maintained at place of use.',
+      de: 'Dank moderner technologischer Lösungen hat unsere Ausrüstung ein extrem langes Wartungsintervall: nur einmal alle zwei Jahre. Die Ausrüstung kann am Einsatzort gewartet werden.'
+    },
     image: '/images/system/features/4.svg'
   },
   {
     id: 4,
-    title: 'Installation in a finished interior',
-    description:
-      'Technological solutions allow to install Liechty systems in a finished interior thanks to flexible high-pressure hoses & wireless control units.',
+    title: {
+      en: 'Installation in a finished interior',
+      de: 'Einbau in einem fertigen Innenraum'
+    },
+    description: {
+      en: 'Technological solutions allow to install Liechty systems in a finished interior thanks to flexible high-pressure hoses & wireless control units.',
+      de: 'Technologische Lösungen ermöglichen den Einbau von Liechty-Systemen in einem fertigen Innenraum dank flexibler Hochdruck-Schlauchleitung und drahtloser Steuergeräten.'
+    },
     image: '/images/system/features/5.svg'
   },
   {
     id: 5,
-    title: 'Alternative air conditioning',
-    description:
-      'The air is cooled by the evaporation of water droplets in it. On average, the temperature is lowered by 2 to 3 degrees, which allows the system to be used for pleasant cooling in summer.',
+    title: {
+      en: 'Alternative air conditioning',
+      de: 'Alternative Klimaanlagen'
+    },
+    description: {
+      en: 'The air is cooled by the evaporation of water droplets in it. On average, the temperature is lowered by 2 to 3 degrees, which allows the system to be used for pleasant cooling in summer.',
+      de: 'Die Luft wird durch die Verdunstung von Wassertröpfchen in der Luft gekühlt. Im Durchschnitt wird die Temperatur um 2 bis 3 Grad gesenkt, so dass das System im Sommer für eine angenehme Kühlung genutzt werden kann.'
+    },
     image: '/images/system/features/6.svg'
   },
   {
     id: 6,
-    title: 'Dust suppression',
-    description:
-      'The optimum microdroplet size is specifically selected for localized dust suppression at the point of atomization.',
+    title: {
+      en: 'Dust suppression',
+      de: 'Staubunterdrückung'
+    },
+    description: {
+      en: 'The optimum microdroplet size is specifically selected for localized dust suppression at the point of atomization.',
+      de: 'Die optimale Größe der Mikrotröpfchen wird speziell für die lokale Staubunterdrückung an der Zerstäubungsstelle ausgewählt.'
+    },
     image: '/images/system/features/7.svg'
   },
   {
     id: 7,
-    title: 'High capacity',
-    description:
-      'Capacity of one nozzle ranges between 0.6 to 1.2 l/h and can humidify the room of max. 50 sq. meters. Just imagine, that capacity of most whole house humidifiers is under 0.3 l/h.',
+    title: {
+      en: 'High capacity',
+      de: 'Hohe Kapazität'
+    },
+    description: {
+      en: 'Capacity of one nozzle ranges between 0.6 to 1.2 l/h and can humidify the room of max. 50 sq. meters. Just imagine, that capacity of most whole house humidifiers is under 0.3 l/h.',
+      de: 'Die Kapazität einer Düse liegt zwischen 0,6 und 1,2 l/h und kann einen Raum von maximal 50 Quadratmetern befeuchten. Stellen Sie sich nur vor, dass die Kapazität der meisten Luftbefeuchter für ganze Häuser unter 0,3 l/h liegt.'
+    },
     image: '/images/system/features/8.svg'
   }
 ]
--- components/pages/system/ui/SystemFeaturesScreen.vue
+++ components/pages/system/ui/SystemFeaturesScreen.vue
@@ -1,13 +1,17 @@
 <script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
+
 import { FeaturesCardsData } from '../model/mock-data'
 import SystemAreasInfoCard from './SystemAreasInfoCard.vue'
+
+const { locale } = useI18n({ useScope: 'global' })
 </script>
 
 <template>
   <section class="py-[140px] xl:py-80 lg:py-60 md:py-40">
     <div class="container">
       <div class="mb-40">
-        <h2 class="section-title">Features</h2>
+        <h2 class="section-title">{{ $t('system.features.title') }}</h2>
       </div>
       <div
         class="grid grid-cols-3 gap-20 xl:gap-50 lg:gap-40 md:grid-cols-1 md:gap-30"
@@ -15,8 +19,8 @@ import SystemAreasInfoCard from './SystemAreasInfoCard.vue'
         <SystemAreasInfoCard
           v-for="card in FeaturesCardsData"
           :key="card.id"
-          :title="card.title"
-          :description="card.description"
+          :title="card.title[locale as 'en' | 'de']"
+          :description="card.description[locale as 'en' | 'de']"
           :image="card.image"
         />
       </div>

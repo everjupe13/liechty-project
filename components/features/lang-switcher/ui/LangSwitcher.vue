966c1ff0bc8b8ee6822ad3f1f3cdeaff4bc4c11a
Ilya Bakirov <everjupe13@gmail.com>
2023-12-07 19:04:46 +0300

    feat: change lang switcher view

--- components/features/lang-switcher/ui/LangSwitcher.vue
+++ components/features/lang-switcher/ui/LangSwitcher.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, onMounted, type Ref, ref } from 'vue'
+import { onMounted, type Ref, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 type LanguageState = 'en' | 'de' | null
@@ -22,21 +22,19 @@ const Languages = [
 
 const savedLanguage: Ref<LanguageState | null> = ref(null)
 const activeLanguageId = ref(Languages[0].id)
-const activeLanguage = computed(() =>
-  Languages.find(lang => lang.id === activeLanguageId.value)
-)
-const handleActiveLanguageId = () => {
-  const currentIndex = Languages.findIndex(
-    lang => lang.id === activeLanguageId.value
-  )
-  const nextActiveIndex =
-    currentIndex < Languages.length - 1 ? currentIndex + 1 : 0
-  const _activeLanguage = Languages.find(lang => lang.id === nextActiveIndex)
 
-  if (window && window instanceof Object && _activeLanguage) {
-    localStorage.setItem(LOCAL_STORAGE_LANG_KEY, _activeLanguage.value)
-    activeLanguageId.value = _activeLanguage.id
-    i18n.locale.value = _activeLanguage.value
+const handleActiveLanguage = (id: number) => {
+  const clickedLanguage = Languages.find(lang => lang.id === id)
+
+  if (
+    window &&
+    window instanceof Object &&
+    activeLanguageId.value !== clickedLanguage?.id &&
+    clickedLanguage
+  ) {
+    localStorage.setItem(LOCAL_STORAGE_LANG_KEY, clickedLanguage.value)
+    activeLanguageId.value = clickedLanguage.id
+    i18n.locale.value = clickedLanguage.value
   }
 }
 
@@ -60,18 +58,27 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
+  <div
+    class="flex items-center gap-x-4 rounded-[9px] border-[2px] border-dark/70"
+  >
     <button
-      v-if="activeLanguage"
+      v-for="lang in Languages"
+      :key="lang.id"
       type="button"
-      class="flex cursor-pointer select-none items-center justify-center rounded-[9px] border-[2px] border-blue bg-transparent px-10 py-3"
-      @click="handleActiveLanguageId"
+      :class="[
+        'm-[-2px] flex cursor-pointer select-none items-center justify-center rounded-[9px] border-[2px] border-transparent bg-transparent px-10 py-3  transition-all',
+        { '!border-blue ': activeLanguageId === lang.id }
+      ]"
+      @click="handleActiveLanguage(lang.id)"
     >
       <span
-        class="uppercase leading-none text-16"
-        :title="activeLanguage.title"
+        :class="[
+          'uppercase leading-none text-dark-text/70 transition-all text-16',
+          { '!text-dark-text': activeLanguageId === lang.id }
+        ]"
+        :title="lang.title"
       >
-        {{ activeLanguage.title }}
+        {{ lang.title }}
       </span>
     </button>
   </div>

a20726d136444e93d9281f58913e109c01d0f337
Ilya Bakirov <everjupe13@gmail.com>
2023-12-22 20:34:50 +0300

    feat: active link classes

--- components/shared/nav-menu/ui/NavMenuItem.vue
+++ components/shared/nav-menu/ui/NavMenuItem.vue
@@ -2,13 +2,20 @@
 import type { NavMenuItemType } from './NavMenuItem.types'
 
 const props = defineProps<NavMenuItemType>()
+const route = useRoute()
+const isActive = computed(
+  () => `/${route.path.slice(1).split('/').slice(1).join('/')}` === props.toLink
+)
 </script>
 
 <template>
   <li
     class="leading-none text-dark-text/70 text-18 hover:text-blue/80 xl:text-16"
   >
-    <NuxtLink :to="props.toLink" :class="linkClasses || ''">
+    <NuxtLink
+      :to="props.toLink"
+      :class="[linkClasses, { 'router-link-active': isActive }]"
+    >
       {{ props.label }}
     </NuxtLink>
   </li>

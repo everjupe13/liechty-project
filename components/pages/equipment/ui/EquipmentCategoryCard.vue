c60ddf0e689f1b8160251000dca3355577e7a666
Ilya Bakirov <everjupe13@gmail.com>
2023-12-22 20:34:38 +0300

    fix: category page mobile

--- components/pages/equipment/ui/EquipmentCategoryCard.vue
+++ components/pages/equipment/ui/EquipmentCategoryCard.vue
@@ -23,27 +23,29 @@ const handleClick = () => {
 
 <template>
   <article
-    class="relative flex h-[410px] items-center justify-center bg-[#F5F6F8] md:h-[220px]"
+    class="relative flex h-[410px] items-center justify-center bg-[#F5F6F8] md:h-auto md:min-h-[220px]"
   >
     <div class="flex h-full w-full gap-x-40 p-20 md:gap-x-15 md:p-13">
-      <div class="flex h-full basis-1/2 items-center justify-center">
+      <div
+        class="flex h-full basis-1/2 items-center justify-center md:basis-1/3"
+      >
         <img
           :src="props.image"
           alt=""
           class="block h-full w-full object-cover"
         />
       </div>
-      <div class="flex basis-1/2 flex-col justify-center">
-        <div class="mb-24">
+      <div class="flex basis-1/2 flex-col justify-center md:basis-2/3">
+        <div class="mb-24 md:mb-10">
           <h3
-            class="mb-14 font-medium leading-tight text-24 md:mb-10 md:text-20"
+            class="mb-14 font-medium leading-tight text-24 md:mb-10 md:text-18"
           >
             {{ props.name }}
           </h3>
-          <div class="prose text-16 md:text-16" v-html="props.description" />
+          <div class="prose text-16 md:text-14" v-html="props.description" />
         </div>
         <div>
-          <AppButton outlined class="gap-x-4" @click="handleClick">
+          <AppButton outlined class="gap-x-4 md:text-16" @click="handleClick">
             <span>{{ $t('home.applications.banner.button') }}</span>
             <ChevronRightArrowIcon />
           </AppButton>

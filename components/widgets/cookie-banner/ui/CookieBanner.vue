2b9c3b9dd893cdef24d4cb51b59554b29fc1313f
Ilya Bakirov <everjupe13@gmail.com>
2024-02-27 03:59:14 +0300

    fix: fix cookie banner

--- components/widgets/cookie-banner/ui/CookieBanner.vue
+++ components/widgets/cookie-banner/ui/CookieBanner.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
 import { AppButton } from '@/components/shared/button'
 
 const LOCAL_STORAGE_STATE_KEY = 'cookie-accepted'
@@ -18,6 +20,40 @@ const confirmCookie = () => {
   localStorage.setItem(LOCAL_STORAGE_STATE_KEY, 'true')
   isVisible.value = false
 }
+
+const LOCALES = {
+  en: {
+    title: 'Cookies',
+    text: `
+      We use cookies to make our website more user-friendly and to
+      continuously improve your web experience. While some of the cookies
+      may be strictly necessary for your usage of the website and its
+      features, others help us to improve your online experience. You can
+      accept all cookies by clicking "I accept" or reject all but the
+      strictly necessary cookies by clicking on "Accept only strictly
+      necessary cookies". To find further information about what cookies we
+      use, please consult our
+    `
+  },
+  de: {
+    title: 'Kekse',
+    text: `
+      Wir verwenden Cookies, um unsere Website benutzerfreundlicher zu gestalten
+      und um Ihr Web-Erlebnis kontinuierlich zu verbessern. Während einige der
+      Cookies für die Nutzung der Website und ihrer Funktionen unbedingt
+      erforderlich sind, helfen uns andere dabei, Ihr Online-Erlebnis zu
+      verbessern. Sie können alle Cookies akzeptieren, indem Sie auf "Ich
+      akzeptiere" klicken, oder alle Cookies bis auf die unbedingt notwendigen
+      ablehnen, indem Sie auf "Nur unbedingt notwendige Cookies akzeptieren"
+      klicken. Weitere Informationen darüber, welche Cookies wir verwenden, finden
+      Sie in unserer
+    `
+  }
+}
+
+const { locale } = useI18n({ useScope: 'global' })
+const isEN = computed(() => locale.value === 'en')
+const currentLocaleData = computed(() => LOCALES[locale.value as 'de' | 'en'])
 </script>
 
 <template>
@@ -27,18 +63,13 @@ const confirmCookie = () => {
         v-if="isVisible"
         class="absolute bottom-20 right-20 h-auto w-full max-w-[560px] rounded-[14px] bg-dark p-20 md:max-w-[calc(100%-32px)]"
       >
-        <h3 class="mb-10 font-bold text-white text-20 md:text-18">Cookies</h3>
+        <h3 class="mb-10 font-bold text-white text-20 md:text-18">
+          {{ currentLocaleData.title }}
+        </h3>
         <p class="mb-20 font-medium text-white text-14">
-          We use cookies to make our website more user-friendly and to
-          continuously improve your web experience. While some of the cookies
-          may be strictly necessary for your usage of the website and its
-          features, others help us to improve your online experience. You can
-          accept all cookies by clicking "I accept" or reject all but the
-          strictly necessary cookies by clicking on "Accept only strictly
-          necessary cookies". To find further information about what cookies we
-          use, please consult our
+          {{ currentLocaleData.text }}
           <NuxtLink href="/privacy-policy" class="text-[#1689f3]">
-            Privacy Policy
+            {{ isEN ? 'Privacy Policy' : 'Datenschutzerklärung' }}
           </NuxtLink>
         </p>
         <div class="flex items-stretch gap-15 md:flex-col">
@@ -49,7 +80,7 @@ const confirmCookie = () => {
               class="!h-full flex-grow !px-20"
               @click="confirmCookie"
             >
-              Accept
+              {{ isEN ? 'Accept' : 'Akzeptieren' }}
             </AppButton>
           </div>
           <div class="flex flex-grow flex-col">
@@ -59,7 +90,11 @@ const confirmCookie = () => {
               class="!h-full flex-grow !px-20"
               @click="confirmCookie"
             >
-              Accept only strictly necessary cookies
+              {{
+                isEN
+                  ? 'Accept only strictly necessary cookies'
+                  : 'Akzeptieren Sie nur notwendige Cookies'
+              }}
             </AppButton>
           </div>
         </div>

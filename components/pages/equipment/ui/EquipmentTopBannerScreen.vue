3aaec186fd1ac0bb31ae1c827c7281f92fd61794
Ilya Bakirov <everjupe13@gmail.com>
2024-03-05 02:52:23 +0300

    feat: add banner on mobile screen

--- components/pages/equipment/ui/EquipmentTopBannerScreen.vue
+++ components/pages/equipment/ui/EquipmentTopBannerScreen.vue
@@ -19,7 +19,7 @@ const subtitleLocaled = computed(() =>
       <div
         class="relative overflow-hidden bg-gray px-80 py-80 pb-[160px] before:absolute before:-inset-90 before:z-[1] before:block before:bg-[url('@/assets/img/bg-mesh.svg')] xl:p-40 md:p-30"
       >
-        <div class="z-[2]">
+        <div class="z-[2] md:pb-[100px] sm:pb-[170px]">
           <div
             class="xl:mb-[160px relative z-[2] mb-25 max-w-[640px] lg:mb-80 md:mb-60"
           >
@@ -45,7 +45,7 @@ const subtitleLocaled = computed(() =>
           </div>
 
           <div
-            class="select-non pointer-events-none absolute bottom-0 right-0 z-[1] w-[790px] xl:w-[600px] lg:hidden"
+            class="pointer-events-none absolute bottom-0 right-0 z-[1] w-[790px] select-none xl:w-[600px] lg:w-[450px] md:w-[300px] sm:w-[280px]"
           >
             <img
               src="/images/equipment/top-banner.png"
